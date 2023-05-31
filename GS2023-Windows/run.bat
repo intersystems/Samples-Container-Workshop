db064bc42858822d1eeef3878df2327c122df4bf
Bob Kuszewski <bob@kuszewski.com>
2023-05-31 11:59:07 -0400

    update run.bat with windows linefeeds

--- GS2023-Windows/run.bat
+++ GS2023-Windows/run.bat
@@ -3,7 +3,7 @@ REM create the directory 'durable' with mode 777
 docker run ^
   --publish 80:52773 ^
   --publish 1972:1972 ^
-  --volume=%CD%\durable:/ISC ^
+  --volume="%CD%\durable:/ISC" ^
   --rm=true ^
   --detach ^
   --env ISC_DATA_DIRECTORY=/ISC/iris.data.d ^
